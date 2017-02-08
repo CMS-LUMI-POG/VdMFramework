86c8a6d5ec6e4a12f97122a7125881097427d145
Joscha Knolle <joscha.knolle@cern.ch>
2017-02-08 19:33:01 +0100

    Implemented per-beam length scale corrections

--- corrections/LengthScale_Corr.py
+++ corrections/LengthScale_Corr.py
@@ -6,8 +6,14 @@ from vdmUtilities import *
 
 class LengthScale_Corr(CorrectionManager.CorrectionProvider):
 
-    LS_ScaleX =-999.    
+    LS_ScaleX =-999.
     LS_ScaleY =-999.
+    LS_ScaleX1 = -999.
+    LS_ScaleY1 = -999.
+    LS_ScaleX2 = -999.
+    LS_ScaleY2 = -999.
+
+    SingleBeamScans = []
 
 
     def GetCorr(self, fileName):
@@ -18,6 +24,12 @@ class LengthScale_Corr(CorrectionManager.CorrectionProvider):
 
         self.LS_ScaleX = float(table["LS_ScaleX"])
         self.LS_ScaleY = float(table["LS_ScaleY"])
+        self.LS_ScaleX1 = float(table["LS_ScaleX1"])
+        self.LS_ScaleY1 = float(table["LS_ScaleY1"])
+        self.LS_ScaleX2 = float(table["LS_ScaleX2"])
+        self.LS_ScaleY2 = float(table["LS_ScaleY2"])
+
+        self.SingleBeamScans = table["SingleBeamScans"]
 
         return
 
@@ -29,7 +41,16 @@ class LengthScale_Corr(CorrectionManager.CorrectionProvider):
         print "PrintLSCorr"
         print "LS_ScaleX ", self.LS_ScaleX
         print "LS_ScaleY ", self.LS_ScaleY
+        print "LS_ScaleX1 ", self.LS_ScaleX1
+        print "LS_ScaleY1 ", self.LS_ScaleY1
+        print "LS_ScaleX2 ", self.LS_ScaleX2
+        print "LS_ScaleY2 ", self.LS_ScaleY2
         print "===="
+        if self.SingleBeamScans:
+            print "PrintSingleBeamScans"
+            for sbs in self.SingleBeamScans:
+                print "Scan "+sbs[0]+": beam "+str(sbs[1])
+            print "===="
         print ""
 
 
@@ -38,29 +59,37 @@ class LengthScale_Corr(CorrectionManager.CorrectionProvider):
         print "Scaling coordinates with lengthscale factors"
 
         self.GetCorr(configFile)
-        
+
         self.PrintCorr()
 
     # apply correction here to coordinate, then write back into entry, check if this really changes value in calling function
 
         for entry in inData:
-            coord = entry.displacement
-            if 'X' in entry.scanName:
-                coord_corr = [a*self.LS_ScaleX for a in coord]
-            if 'Y' in entry.scanName:
-                coord_corr = [a*self.LS_ScaleY for a in coord]
+            correction = 1.0
+            for sbs in self.SingleBeamScans:
+                if sbs[0] in entry.scanName:
+                    if 'X' in sbs[0] and sbs[1] == 1:
+                        correction = self.LS_ScaleX1
+                    elif 'Y' in sbs[0] and sbs[1] == 1:
+                        correction = self.LS_ScaleY1
+                    elif 'X' in sbs[0] and sbs[1] == 2:
+                        correction = self.LS_ScaleX2
+                    elif 'Y' in sbs[0] and sbs[1] == 2:
+                        correction = self.LS_ScaleY2
+                    else:
+                        continue
+                    break
+            else:
+                if 'X' in entry.scanName:
+                    correction = self.LS_ScaleX
+                elif 'Y' in entry.scanName:
+                    correction = self.LS_ScaleY
 
+            coord = entry.displacement
+            coord_corr = [a*correction for a in coord]
             entry.displacement = coord_corr
 
-            if 'X' in entry.scanName:
-                for bx in entry.collidingBunches:
-                    coordinates = entry.spPerBX[bx]
-                    coordinates_corrected = [ a*self.LS_ScaleX for a in coordinates]
-                    entry.spPerBX[bx] = coordinates_corrected
-            if 'Y' in entry.scanName:
-                for bx in entry.collidingBunches:
-                    coordinates = entry.spPerBX[bx]
-                    coordinates_corrected = [ a*self.LS_ScaleY for a in coordinates]
-                    entry.spPerBX[bx] = coordinates_corrected
-#            entry.spPerBx = coordPerBX_corr
-
+            for bx in entry.collidingBunches:
+                coordinates = entry.spPerBX[bx]
+                coordinates_corrected = [a*correction for a in coordinates]
+                entry.spPerBX[bx] = coordinates_corrected

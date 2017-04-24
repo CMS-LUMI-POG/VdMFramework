04d769e673e0245f0da42e3782558d51dcf705dc
anbabaev <anbabaev@cern.ch>
2017-04-24 17:45:12 +0700

    Add files via upload

--- makeScanFileII.py
+++ makeScanFileII.py
@@ -47,6 +47,10 @@ def doMakeScanFile(ConfigInfo):
     columnsList = ['fill', 'run', 'ls', 'nb', 'sec', 'msec', 'acqflag', 'step', 'beam', 'ip', 'scanstatus', 'plane', 'progress', 'nominal_separation', 'read_nominal_B1sepPlane', 'read_nominal_B1xingPlane', 'read_nominal_B2sepPlane', 'read_nominal_B2xingPlane', 'set_nominal_B1sepPlane', 'set_nominal_B1xingPlane', 'set_nominal_B2sepPlane', 'set_nominal_B2xingPlane', 'bpm_5LDOROS_B1Names', 'bpm_5LDOROS_B1hPos', 'bpm_5LDOROS_B1vPos,bpm_5LDOROS_B1hErr', 'bpm_5LDOROS_B1vErr,bpm_5RDOROS_B1Names', 'bpm_5RDOROS_B1hPos', 'bpm_5RDOROS_B1vPos', 'bpm_5RDOROS_B1hErr', 'bpm_5RDOROS_B1vErr', 'bpm_5LDOROS_B2Names', 'bpm_5LDOROS_B2hPos', 'bpm_5LDOROS_B2vPos', 'bpm_5LDOROS_B2hErr', 'bpm_5LDOROS_B2vErr', 'bpm_5RDOROS_B2Names', 'bpm_5RDOROS_B2hPos', 'bpm_5RDOROS_B2vPos', 'bpm_5RDOROS_B2hErr', 'bpm_5RDOROS_B2vErr', 'atlas_totInst', 'nominal_separation_plane']
 
     extractedList = df.columns.values.tolist()
+    if('nominal_separation_plane' in extractedList):
+        print "nominal_separation_plane in extractedList: this column is used"
+    else:
+        print "nominal_separation_plane not in extractedList: plane column is used"
 
     #if not (columnsList == extractedList):
        # print "Attention: First line in dip csv file not as expected, check file integrity. Exit program."
@@ -57,9 +61,6 @@ def doMakeScanFile(ConfigInfo):
     if (Fill != str(FillfromDip)):
             print("Mismatch between fill info from dip and from config file. Exit program.")
             sys.exit(1)
-
-            
-
     
 # check that there is only one fill number in file
 
@@ -77,30 +78,36 @@ def doMakeScanFile(ConfigInfo):
         print "Attention: There appears to be more than one run number in the dip file. Is this intentional ?"
         print "List of all runs in dip file: ", df['run'].drop_duplicates().tolist()
 
+    #fill=int(Fill)
+
     scan = [ [] for entry in ScanNames]
     Run = [0 for entry in ScanNames]
     for i, scanName in enumerate(ScanNames):
         print "Now at scan", scanName
         timeWindow = [ScanTimeWindows[i][0], ScanTimeWindows[i][1]]
 
-
 # get scan point info from dip file
 
         dfPreSelect = df[(df.sec >= timeWindow[0]) & (df.sec <= timeWindow[1]) & (df.ip == 32) & (df.acqflag == 1) & (df.scanstatus == 'ACQUIRING') & (df.step != 9999)]
 
+        if('nominal_separation_plane' in extractedList):
+            SeparationPlane=dfPreSelect.nominal_separation_plane
+        else:
+            SeparationPlane=dfPreSelect.plane
+
 # make sure that preselected df contains at most one X (=CROSSING) and one Y (=SEPARATION) scan
 
         justonescan = False
-        if len(dfPreSelect[dfPreSelect.nominal_separation_plane == "CROSSING"].index.tolist()) == 0:
+        if len(dfPreSelect[SeparationPlane == "CROSSING"].index.tolist()) == 0:
             justonescan = True
-        if len(dfPreSelect[dfPreSelect.nominal_separation_plane == "SEPARATION"].index.tolist()) == 0:
+        if len(dfPreSelect[SeparationPlane == "SEPARATION"].index.tolist()) == 0:
             justonescan = True
 
         if not(justonescan):
-            minIndexX = dfPreSelect[dfPreSelect.nominal_separation_plane == "CROSSING"].index.min()
-            maxIndexX = dfPreSelect[dfPreSelect.nominal_separation_plane == "CROSSING"].index.max()
-            minIndexY = dfPreSelect[dfPreSelect.nominal_separation_plane == "SEPARATION"].index.min()
-            maxIndexY = dfPreSelect[dfPreSelect.nominal_separation_plane == "SEPARATION"].index.max()
+            minIndexX = dfPreSelect[SeparationPlane == "CROSSING"].index.min()
+            maxIndexX = dfPreSelect[SeparationPlane == "CROSSING"].index.max()
+            minIndexY = dfPreSelect[SeparationPlane == "SEPARATION"].index.min()
+            maxIndexY = dfPreSelect[SeparationPlane == "SEPARATION"].index.max()
             if (minIndexX < minIndexY and maxIndexX > maxIndexY) or (minIndexX > minIndexY and maxIndexX < maxIndexY):
                 print "Attention: Time search window given in config file contains more than one X and one Y scan, do not know how to handle this. Exit program"
                 sys.exit(1)
@@ -108,11 +115,9 @@ def doMakeScanFile(ConfigInfo):
         nomSep = []
         dfSP = pd.DataFrame()
         if ("X" in scanName):
-            dfSP = dfPreSelect[dfPreSelect.nominal_separation_plane == "CROSSING"]
-            
+            dfSP = dfPreSelect[SeparationPlane == "CROSSING"]           
         if ("Y" in scanName):
-            dfSP = dfPreSelect[dfPreSelect.nominal_separation_plane == "SEPARATION"]
-
+            dfSP = dfPreSelect[SeparationPlane == "SEPARATION"]
 
 # cut off zero separation points at very beginning and very end of scan            
         
